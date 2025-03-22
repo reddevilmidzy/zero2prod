83e4b24c8231df7ebe0e0db860c1edd508e1cdfb
reddevilmidzy <midzy0228@gmail.com>
2025-03-23 00:31:11 +0900

    feat: refactor server to use dynamic port binding for health check

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
+use std::net::TcpListener;
 use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    run()?.await
+    let listener = TcpListener::bind("localhost:8080").expect("Failed to bind port");
+    run(listener)?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,11 +1,12 @@
+use std::net::TcpListener;
+
 #[tokio::test]
 async fn health_check() {
-    spawn_app();
-
+    let address = spawn_app();
     let client = reqwest::Client::new();
 
     let response = client
-        .get("http://localhost:8080/health_check")
+        .get(&format!("{}/health_check", &address))
         .send()
         .await
         .expect("Failed to send request");
@@ -14,10 +15,16 @@ async fn health_check() {
     assert_eq!(Some(0), response.content_length());
 }
 
-fn spawn_app() {
-    let server = zero2prod::run().expect("Failed to start zero2prod");
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("localhost:0").expect("Failed to bind random port");
+
+    // OS가 할당한 포트 추출
+    let port = listener.local_addr().unwrap().port();
+    let server = zero2prod::run(listener).expect("Failed to bind address");
     // 서버를 백그라운드로 구동
     // torio::spawn은 생성된 퓨처에 대한 핸들을 반환한다.
     // 하지만 여기에서는 사용하지 않으므로 바인딩하진 않음
     let _ = tokio::spawn(server);
+    //애플리케이션 주소를 호출자에게 반환
+    format!("http://localhost:{}", port)
 }
