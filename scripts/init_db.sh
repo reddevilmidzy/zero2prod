61a1d378beb37da1cac54d38bdb2f9c21b6c922c
reddevilmidzy <midzy0228@gmail.com>
2025-03-24 22:38:36 +0900

    feat: add checks for psql and sqlx installation in init_db script

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -2,6 +2,19 @@
 set -x
 set -eo pipefail
 
+if ! [ -x "$(*command -v psql)" ]; then
+  echo >&2 "Error: psql is not installed."
+  exit 1
+fi
+
+if ! [ -x "$(command -v sqlx)" ]; then
+  echo >&2 "Error: sqlx is not installed."
+  echo >&2 "Use:"
+  echo >&2 "    cargo install --version='~0.8' sqlx-cli --no-default-features --features rustls,postgres"
+  echo >&2 "to install it."
+  exit 1
+fi
+
 DB_USER="${POSTGRES_USER:=postgres}"
 DB_PASSWORD="${POSTGRES_PASSWORD:=password}"
 DB_NAME="${POSTGRES_DB:=newsletter}"
