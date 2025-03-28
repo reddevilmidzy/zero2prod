4dfddf66adf33304e1585f58aa5bf5bab499b079
reddevilmidzy <midzy0228@gmail.com>
2025-03-28 21:24:25 +0900

    feat: enhance logging setup with configurable output sink for tracing subscriber

--- src/telemetry.rs
+++ src/telemetry.rs
@@ -2,16 +2,25 @@ use tracing::Subscriber;
 use tracing::subscriber::set_global_default;
 use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
 use tracing_log::LogTracer;
+use tracing_subscriber::fmt::MakeWriter;
 use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::{EnvFilter, Registry};
 
-pub fn get_subscriber(name: String, env_filter: String) -> impl Subscriber + Send + Sync {
+pub fn get_subscriber<Sink>(
+    name: String,
+    env_filter: String,
+    sink: Sink,
+) -> impl Subscriber + Sync + Send
+where
+    // https://doc.rust-lang.org/nomicon/hrtb.html를 참고해보자.
+    Sink: for<'a> MakeWriter<'a> + Send + Sync + 'static,
+{
     // RUST_LOG 환경 변수가 설정되어 있지 않으면 info 및 그 이상의 레벨의 모든 span을 출력
     let env_filter =
         EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new(env_filter));
 
     // 포맷이 적용된 span들을 stdout으로 출력
-    let formatting_layer = BunyanFormattingLayer::new(name, std::io::stdout);
+    let formatting_layer = BunyanFormattingLayer::new(name, sink);
 
     // `with` 메서드는 `SubscriberExt`에서 제공. 이 녀석은 `Subscriber`의 확장 트레이트며, `tracing_subscriber`에 의해 노출됨
     Registry::default()
