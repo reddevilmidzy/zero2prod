f3b12e251b4d9cae1ff0730c9329b1e48c036216
reddevilmidzy <midzy0228@gmail.com>
2025-04-02 21:41:17 +0900

    feat: improve subscriber name parsing with error handling in subscription function

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -2,7 +2,6 @@ use crate::domain::{NewSubscriber, SubscriberName};
 use actix_web::{HttpResponse, web};
 use chrono::Utc;
 use sqlx::PgPool;
-use unicode_segmentation::UnicodeSegmentation;
 use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
@@ -20,9 +19,13 @@ pub struct FormData {
     )
 )]
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
+    let name = match SubscriberName::parse(form.0.name) {
+        Ok(name) => name,
+        Err(_) => return HttpResponse::BadRequest().body("Invalid subscriber name"),
+    };
     let new_subscriber = NewSubscriber {
         email: form.0.email,
-        name: SubscriberName::parse(form.0.name).expect("Name validation failed."),
+        name
     };
     match insert_subscriber(&pool, &new_subscriber).await {
         Ok(_) => HttpResponse::Ok().finish(),
