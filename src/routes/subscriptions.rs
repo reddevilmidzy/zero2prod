50ac2b20635914c9aa683c093958a77beed6fccb
reddevilmidzy <midzy0228@gmail.com>
2025-04-02 21:33:25 +0900

    feat: enhance SubscriberName validation and add comprehensive tests

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -30,17 +30,6 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
     }
 }
 
-pub fn is_valid_name(s: &str) -> bool {
-    let is_empty_or_whitespace = s.trim().is_empty();
-
-    let is_too_long = s.graphemes(true).count() > 256;
-
-    let forbidden_characters = ['/', '(', ')', '"', '<', '>', '\\', '{', '}'];
-    let contains_forbidden_characters = s.chars().any(|c| forbidden_characters.contains(&c));
-
-    !(is_empty_or_whitespace || is_too_long || contains_forbidden_characters)
-}
-
 #[tracing::instrument(name = "Adding a new subscriber", skip(new_subscriber, pool))]
 pub async fn insert_subscriber(
     pool: &PgPool,
