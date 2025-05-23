a286d75e44f9f90f9a5d6f26bb01fa9f4376f7d5
reddevilmidzy <midzy0228@gmail.com>
2025-05-23 20:28:35 +0900

    feat: add basic authentication to newsletter publishing and handle unauthorized requests

--- tests/api/newsletter.rs
+++ tests/api/newsletter.rs
@@ -95,6 +95,32 @@ async fn newsletters_are_delivered_to_confirmed_subscribers() {
     // Mock은 뉴스레터 이메일을 보냈다는 drop을 검증
 }
 
+#[tokio::test]
+async fn request_missing_authorization_are_rejected() {
+    // Arrange
+    let app = spawn_app().await;
+
+    let response = reqwest::Client::new()
+        .post(&format!("{}/newsletters", &app.address))
+        .json(&serde_json::json!({
+            "title": "Newsletter title",
+            "content": {
+                "text": "Newsletter body as plain text",
+                "html": "<p>Newsletter body as HTML</p>",
+            }
+        }))
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    // Assert
+    assert_eq!(401, response.status().as_u16());
+    assert_eq!(
+        r#"Basic realm="publish""#,
+        response.headers()["WWW-Authenticate"]
+    );
+}
+
 async fn create_unconfirmed_subscriber(app: &TestApp) -> ConfirmationLinks {
     let body = "name=redddy&email=areyou@redddy.com";
 
