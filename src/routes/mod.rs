b2e903f1ff9f74c37ba41cfcbe5a3224324edcd7
reddevilmidzy <midzy0228@gmail.com>
2025-05-25 17:40:21 +0900

    feat: implement login form and authentication routes

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,6 +1,7 @@
 mod greet;
 mod health_check;
 mod home;
+mod login;
 mod newsletters;
 mod subscriptions;
 mod subscriptions_confirm;
@@ -8,6 +9,7 @@ mod subscriptions_confirm;
 pub use greet::*;
 pub use health_check::*;
 pub use home::*;
+pub use login::*;
 pub use newsletters::*;
 pub use subscriptions::*;
 pub use subscriptions_confirm::*;
