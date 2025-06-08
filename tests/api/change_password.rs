7ffa6ea720aad3e7e458f1fb1a4118d7e932cce5
reddevilmidzy <midzy0228@gmail.com>
2025-06-08 21:35:38 +0900

    feat: add validation for matching new password fields in change password functionality

--- tests/api/change_password.rs
+++ tests/api/change_password.rs
@@ -31,3 +31,36 @@ async fn you_must_be_logged_in_to_change_your_password() {
     // Assert
     assert_is_redirect_to(&response, "/login");
 }
+
+#[tokio::test]
+async fn new_password_fields_must_match() {
+    // Arrange
+    let app = spawn_app().await;
+    let new_password = Uuid::new_v4().to_string();
+    let another_new_password = Uuid::new_v4().to_string();
+
+    // Act - Part 1 - 로그인
+    app.post_login(&serde_json::json!({
+        "username": &app.test_user.username,
+        "password": &app.test_user.password,
+    }))
+    .await;
+
+    // Act - Part 2 - 비밀번호 변경을 시도한다.
+    let response = app
+        .post_change_password(&serde_json::json!({
+            "current_password": &app.test_user.password,
+            "new_password": &new_password,
+            "new_password_check": &another_new_password,
+        }))
+        .await;
+
+    assert_is_redirect_to(&response, "/admin/password");
+
+    // Act - Part 3 - 리다이렉트를 따른다.
+    let html_page = app.get_change_password_html().await;
+
+    assert!(html_page.contains(
+        "<p><i>You entered two different new passwords - the field values must match.</i></p>"
+    ))
+}
