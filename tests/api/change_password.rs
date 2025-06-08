91ce0fa836f2c3a59d822121a90be7f47e17f597
reddevilmidzy <midzy0228@gmail.com>
2025-06-08 21:50:40 +0900

    feat: add validation for current password in change password functionality

--- tests/api/change_password.rs
+++ tests/api/change_password.rs
@@ -64,3 +64,34 @@ async fn new_password_fields_must_match() {
         "<p><i>You entered two different new passwords - the field values must match.</i></p>"
     ))
 }
+
+#[tokio::test]
+async fn current_password_must_be_valid() {
+    // Arrange
+    let app = spawn_app().await;
+    let new_password = Uuid::new_v4().to_string();
+    let wrong_password = Uuid::new_v4().to_string();
+
+    // Act - Part 1 - 로그인
+    app.post_login(&serde_json::json!({
+        "username": &app.test_user.username,
+        "password": &app.test_user.password,
+    }))
+    .await;
+
+    // Act - Part 2 - 비밀번호를 변경한다.
+    let response = app
+        .post_change_password(&serde_json::json!({
+            "current_password": &wrong_password,
+            "new_password": &new_password,
+            "new_password_check": &new_password,
+        }))
+        .await;
+
+    // Assert
+    assert_is_redirect_to(&response, "/admin/password");
+
+    // Act - Part 3 - 리다이렉트를 따른다.
+    let html_page = app.get_change_password_html().await;
+    assert!(html_page.contains("<p><i>The current password is incorrect.</i></p>"));
+}
